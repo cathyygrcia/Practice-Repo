c83b2c4fa204d28402e2c4cb6a0a54f72cb49333
Cathy Garcia <garciacathy67@yahoo.com>
2023-12-13 21:03:29 +0000

    Practicing ternary operators

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,7 @@
 import './App.css';
 
 export default function App() {
-  console.log(practice('cAthY'));
-}
-
-function practice(string) {
-  const firstChar = string[0].toUpperCase();
-  const restOfWord = string.slice(1).toLowerCase();
-  return firstChar + restOfWord;
+  const username = 'Cathy';
+  const defaultName = username || 'Stranger';
+  console.log(defaultName);
 }
