6cb573e6a73866fe6b196b71b9e2b8b96a907a4e
Cathy Garcia <garciacathy67@yahoo.com>
2023-12-13 18:59:08 +0000

    Practicing manipulations problems

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,11 @@
-import { useEffect, useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
 export default function App() {
-  const [serverData, setServerData] = useState('');
+  console.log(practice('cAthY'));
+}
 
-  useEffect(() => {
-    async function readServerData() {
-      const resp = await fetch('/api/hello');
-      const data = await resp.json();
-
-      console.log('Data from server:', data);
-
-      setServerData(data.message);
-    }
-
-    readServerData();
-  }, []);
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>{serverData}</h1>
-    </>
-  );
+function practice(string) {
+  const firstChar = string[0].toUpperCase();
+  const restOfWord = string.slice(1).toLowerCase();
+  return firstChar + restOfWord;
 }
