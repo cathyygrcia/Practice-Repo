ed6f44f691b0f3dc457ac2c9f445b28790a5d0a3
Cathy Garcia <garciacathy67@yahoo.com>
2023-12-16 21:00:55 +0000

    Reviewing objects

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,19 @@
 import './App.css';
 
-export default function App() {
-  const names = ['Cathy', 'Katy', 'Anna', 'Vane', 'Gen', 'Valerie'];
+type Person = {
+  name: string;
+  age: number;
+  profession: string;
+  car: string;
+};
 
-  for (let i = 1; i < names.length; i++) {
-    console.log(names[i]);
-  }
+export default function App() {
+  const person: Person = {
+    name: 'Cathy',
+    age: 27,
+    profession: 'software engineer',
+    car: 'Subaru',
+  };
+
+  console.log(person['name']);
 }
