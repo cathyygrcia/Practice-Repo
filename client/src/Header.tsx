2f7dc219df8c4ae2782df7ff327cd5b3e2cd7c35
Cathy Garcia <garciacathy67@yahoo.com>
2023-12-18 20:57:30 +0000

    Reviewing for in loops and created official header

--- client/src/Header.tsx
+++ client/src/Header.tsx
@@ -1,7 +1,48 @@
 export default function Header() {
+  function loops() {
+    const spaceship = {
+      crew: {
+        captain: {
+          name: 'Lily',
+          degree: 'Computer Engineering',
+          cheerTeam() {
+            console.log('You got this!');
+          },
+        },
+        'chief officer': {
+          name: 'Dan',
+          degree: 'Aerospace Engineering',
+          agree() {
+            console.log('I agree, captain!');
+          },
+        },
+        medic: {
+          name: 'Clementine',
+          degree: 'Physics',
+          announce() {
+            console.log(`Jets on!`);
+          },
+        },
+        translator: {
+          name: 'Shauna',
+          degree: 'Conservation Science',
+          powerFuel() {
+            console.log('The tank is full!');
+          },
+        },
+      },
+    };
+
+    for (const crewMember in spaceship.crew) {
+      console.log(`${crewMember}: ${spaceship.crew[crewMember].name}`);
+    }
+  }
+  loops();
   return (
     <>
-      <h2 className="header">Cathy's Practice Website</h2>
+      <div className="header">
+        <h2 className="header-title">Cathy's Practice Space</h2>
+      </div>
     </>
   );
 }
