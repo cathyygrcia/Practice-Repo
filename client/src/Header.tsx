aa8b86ef8504aedc0afc83af96d7cef0f60d5428
Cathy Garcia <garciacathy67@yahoo.com>
2023-12-18 21:39:45 +0000

    objects practice

--- client/src/Header.tsx
+++ client/src/Header.tsx
@@ -1,40 +1,20 @@
 export default function Header() {
   function loops() {
-    const spaceship = {
-      crew: {
-        captain: {
-          name: 'Lily',
-          degree: 'Computer Engineering',
-          cheerTeam() {
-            console.log('You got this!');
-          },
-        },
-        'chief officer': {
-          name: 'Dan',
-          degree: 'Aerospace Engineering',
-          agree() {
-            console.log('I agree, captain!');
-          },
-        },
-        medic: {
-          name: 'Clementine',
-          degree: 'Physics',
-          announce() {
-            console.log(`Jets on!`);
-          },
-        },
-        translator: {
-          name: 'Shauna',
-          degree: 'Conservation Science',
-          powerFuel() {
-            console.log('The tank is full!');
-          },
-        },
+    const people = {
+      friends: {
+        name: 'Cathy',
+        age: 27,
+        career: 'Software Engineer',
+      },
+      'other-friends': {
+        name: 'Anna',
+        age: 28,
+        career: 'Pilates',
       },
     };
 
-    for (const crewMember in spaceship.crew) {
-      console.log(`${crewMember}: ${spaceship.crew[crewMember].name}`);
+    for (const key in people) {
+      console.log(people[key].career);
     }
   }
   loops();
