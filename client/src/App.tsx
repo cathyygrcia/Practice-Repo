71a9531a5b4e385817b1aa8597279e96a6309097
Cathy Garcia <garciacathy12@gmail.com>
2023-12-29 20:42:44 +0000

    Working on sharing state between components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,21 @@ import Header from './Header';
 import { Routes, Route } from 'react-router-dom';
 import Concerts from './Concerts';
 import Running from './Running';
+import { useState } from 'react';
 
 export default function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function onHandleClick() {
+    setIsOpen(!isOpen);
+  }
   return (
     <>
       <Routes>
-        <Route path="" element={<Header />}>
-          <Route path="Concerts" element={<Concerts />} />
+        <Route
+          path=""
+          element={<Header isOpen={isOpen} handleClick={onHandleClick} />}>
+          <Route path="Concerts" element={<Concerts isOpen={isOpen} />} />
           <Route path="Running" element={<Running />} />
         </Route>
       </Routes>
