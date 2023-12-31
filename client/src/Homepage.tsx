f99c2befde787cde82bc42d77e0b06c36c8c60a3
Cathy Garcia <garciacathy12@gmail.com>
2023-12-31 00:12:06 +0000

    finally fixed the images portion of the homepage.

--- client/src/Homepage.tsx
+++ client/src/Homepage.tsx
@@ -1,3 +1,23 @@
 export default function Homepage() {
-  return <></>;
+  return (
+    <>
+      <div className="images-row">
+        <div className="column-third">
+          <div className="image-wrapper">
+            <img src="/images/cathyg2.png" className="image" />
+          </div>
+        </div>
+        <div className="column-third">
+          <div className="image-wrapper">
+            <img src="/images/cathy.jpg" className="image" />
+          </div>
+        </div>
+        <div className="column-third">
+          <div className="image-wrapper">
+            <img src="/images/cathyg.png" className="image" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
