e94ea96676e8f0d25bb2f69320b6e034224f3ddb
Cathy Garcia <garciacathy12@gmail.com>
2024-01-09 22:03:05 +0000

    Cleaning up styling

--- client/src/Music.tsx
+++ client/src/Music.tsx
@@ -1,28 +1,64 @@
 import { useEffect, useState } from 'react';
-import { FaChevronLeft } from 'react-icons/fa';
-import { FaChevronRight } from 'react-icons/fa';
 
-const carouselImages = ['/images/TheSkints.jpeg', '/images/LilaIke.jpeg'];
+const carouselImages1 = [
+  '/images/TheSkints.jpeg',
+  '/images/LilaIke.jpeg',
+  '/images/Interrupters.jpeg',
+];
+const carouselImages2 = [
+  '/images/TribalSeeds.jpeg',
+  '/images/Protoje.jpeg',
+  '/images/TheSpecials.jpeg',
+];
 
 export default function Music() {
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentIndex1, setCurrentIndex1] = useState(0);
+  const [currentIndex2, setCurrentIndex2] = useState(0);
 
-  function handleRightArrow() {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % carouselImages.length);
+  function handleCarousel1() {
+    setCurrentIndex1((prevIndex) => (prevIndex + 1) % carouselImages1.length);
   }
-  useEffect(() => {
-    const timeoutId = setTimeout(handleRightArrow, 3000); // Adjust the delay as needed (in milliseconds)
 
-    // Cleanup the timeout to avoid potential issues
-    return () => clearTimeout(timeoutId);
-  }, [currentIndex]); // Run this effect whenever currentIndex changes
+  function handleCarousel2() {
+    setCurrentIndex2((prevIndex) => (prevIndex + 1) % carouselImages2.length);
+  }
+
+  useEffect(() => {
+    const timeoutId1 = setTimeout(handleCarousel1, 2000);
+
+    return () => clearTimeout(timeoutId1);
+  }, [currentIndex1]);
+
+  useEffect(() => {
+    const timeoutId2 = setTimeout(handleCarousel2, 2000);
+
+    return () => clearTimeout(timeoutId2);
+  }, [currentIndex2]);
 
   return (
     <>
+      <div className="carousel-row">
+        <div className="carousel-container">
+          <div className="carousel-third">
+            <img
+              src={carouselImages1[currentIndex1]}
+              className="test"
+              alt={`music-${currentIndex1}`}
+            />
+          </div>
+          <div className="carousel-third">
+            <img
+              src={carouselImages2[currentIndex2]}
+              className="test"
+              alt={`music-${currentIndex2}`}
+            />
+          </div>
+        </div>
+      </div>
       <div className="row">
         <div className="column-fourth">
           <div className="boxes white">
-            <img src="/images/TribalSeeds.jpeg" className="music-image" />
+            <img src="/images/IyaTerra.jpeg" className="music-image" />
           </div>
         </div>
         <div className="column-fourth">
@@ -37,23 +73,8 @@ export default function Music() {
         </div>
         <div className="column-fourth">
           <div className="boxes white">
-            <img src="/images/Protoje.jpeg" className="music-image" />
+            <img src="/images/FortunateYouth.jpeg" className="music-image" />
           </div>
-        </div>
-      </div>
-      <div className="carousel-container">
-        <div className="carousel-third">
-          <FaChevronLeft className="left-arrow" />
-        </div>
-        <div className="carousel-third">
-          <img
-            src={carouselImages[currentIndex]}
-            className="test"
-            alt={`music-${currentIndex}`}
-          />
-        </div>
-        <div className="carousel-third" onClick={handleRightArrow}>
-          <FaChevronRight className="right-arrow" />
         </div>
       </div>
     </>
