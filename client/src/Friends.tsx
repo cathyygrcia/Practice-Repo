2facf2dadb94073f586d468f207e4f2502478f6b
Cathy Garcia <garciacathy12@gmail.com>
2024-01-12 22:37:46 +0000

    Working on friends component

--- client/src/Friends.tsx
+++ client/src/Friends.tsx
@@ -1,3 +1,13 @@
 export default function Friends() {
-  return <p className="white">Testing</p>;
+  return (
+    <>
+      <p className="friends">
+        This is going to be my component for pictures with friends. I also wish
+        to implement a form section to others can leave notes on my page.
+      </p>
+      <p className="friends">
+        I also want to practice more css and need to focus on public storage.
+      </p>
+    </>
+  );
 }
